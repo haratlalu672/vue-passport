19f398f6aaf19877a554a971fa54a7e3696f8520
haratlalu672 <ndaharatlo672@gmail.com>
2020-12-05 07:26:02 +0800

    frontend rest api

---
+++ src/components/Navigation.vue
@@ -0,0 +1,39 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <router-link class="navbar-brand" exact :to="{ name: 'dashboard' }"
+      >Navbar</router-link
+    >
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarNav"
+      aria-controls="navbarNav"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <router-link :to="{ name: 'register' }" class="nav-link"
+            >REGISTER</router-link
+          >
+        </li>
+        <li class="nav-item">
+          <router-link :to="{ name: 'login' }" class="nav-link"
+            >LOGIN</router-link
+          >
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,20 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+//import router, bootstrap, popper, jQuery
+import router from './router'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'jquery/dist/jquery.min'
+import 'popper.js/dist/popper.min'
+import 'bootstrap/dist/js/bootstrap.min'
+
+//menggunakan vue router
+
+const app = createApp(App)
+
+//use vue router
+app.use(router)
+
+app.mount('#app')
+
+
